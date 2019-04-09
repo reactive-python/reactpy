e832b7e276c3a871bb381878306a3ab978788252
Ryan Morshead <ryan.morshead@gmail.com>
2019-04-08 19:20:25 -0700

    fix #14 and add some tests (#17)

--- src/py/idom/bunch.py
+++ src/py/idom/bunch.py
@@ -9,6 +9,9 @@ class StaticBunch(Mapping):
     def __init__(self, *args, **kwargs):
         self.__dict__.update(*args, **kwargs)
 
+    def __contains__(self, name):
+        return name in self.__dict__
+
     def __len__(self) -> int:
         return len(self.__dict__)
 
@@ -22,7 +25,10 @@ class StaticBunch(Mapping):
         return repr(self.__dict__)
 
     def __setattr__(self, name, value):
-        raise TypeError("%r is immutable." % self)
+        raise TypeError("%r is immutable." % type(self).__name__)
+
+    def __delattr__(self, name):
+        raise TypeError("%r is immutable." % type(self).__name__)
 
 
 class DynamicBunch(MutableMapping, StaticBunch):
@@ -35,4 +41,7 @@ class DynamicBunch(MutableMapping, StaticBunch):
         del self.__dict__[name]
 
     def __setattr__(self, name, value):
-        object.__setattr__(self, name, value)
+        self[name] = value
+
+    def __delattr__(self, name):
+        del self[name]
