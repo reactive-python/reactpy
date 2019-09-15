65c3de8c2aa23760ff8b0d4b01366cc2c2f429ef
rmorshea <ryan.morshead@gmail.com>
2019-09-14 19:49:29 -0700

    fix docs logo

--- src/py/docs/source/conf.py
+++ src/py/docs/source/conf.py
@@ -47,7 +47,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["templates"]
+# templates_path = ["templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -98,7 +98,7 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["static"]
+# html_static_path = ["static"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
