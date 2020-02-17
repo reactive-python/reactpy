e3bb7c7cdd89b1a65afc392ce2adf83fb04a7a39
rmorshea <ryan.morshead@gmail.com>
2020-02-16 23:06:00 -0800

    move conf files into setup.cfg

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,5 +4,5 @@ set -e
 pytest src/py/tests --headless --cov=idom --cov-fail-under=82
 black --verbose --check src/py
 flake8 src/py
-mypy src/py/idom --config-file=src/py/mypy.ini
-sphinx-build -b html src/py/docs/source src/py/docs/build
+mypy src/py/idom
+sphinx-build -W -b html src/py/docs/source src/py/docs/build
