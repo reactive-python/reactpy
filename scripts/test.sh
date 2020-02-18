5792e253167c6466b47c7e11f88de992d3863cbb
rmorshea <ryan.morshead@gmail.com>
2020-02-17 16:40:02 -0800

    allow warnings in sphinx builds

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,4 +5,4 @@ pytest src/py/tests --headless --cov=idom --cov-fail-under=82
 black --verbose --check src/py
 flake8 src/py
 mypy src/py/idom
-sphinx-build -W -b html src/py/docs/source src/py/docs/build
+sphinx-build -b html src/py/docs/source src/py/docs/build
