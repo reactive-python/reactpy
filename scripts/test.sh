b43e79a40cec5e1760bf0b78eff4b5e81e4743eb
rmorshea <ryan.morshead@gmail.com>
2020-03-08 14:05:27 -0700

    rerun flaky tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-pytest src/py/tests --headless --cov=idom --cov-fail-under=82
+pytest src/py/tests --headless --cov=idom --cov-fail-under=82 --reruns 3
 black --verbose --check src/py
 flake8 src/py
 mypy src/py/idom
