3a5374b359f8cb5477013ac5e5ef171b0c557601
rmorshea <ryan.morshead@gmail.com>
2020-03-08 18:01:05 -0700

    initial reorganization

--- src/js/config-overrides.js
+++
@@ -1,9 +0,0 @@
-module.exports = (config, env) => {
-  config.optimization.runtimeChunk = false;
-  config.optimization.splitChunks = {
-    cacheGroups: {
-      default: false,
-    },
-  };
-  return config;
-};
--- src/js/packages/idom-jupyter-widget-hook/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Layout from "idom-layout";
-
-window.iDomWidgetMount = function iDomWidgetMount(endpoint, mountId) {
-    const mount = document.getElementById(mountId);
-    ReactDOM.render(<Layout endpoint={endpoint} />, mount);
-};
