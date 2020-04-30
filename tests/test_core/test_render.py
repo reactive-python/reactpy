73da176a0195f3101573111a585618fb7e52d63f
rmorshea <ryan.morshead@gmail.com>
2020-04-30 01:27:34 -0700

    test renderer
    tests pass but logs show a task is not getting cancelled appropriately
    when the loop closes

---
+++ tests/test_core/test_render.py
@@ -0,0 +1,78 @@
+import asyncio
+import pytest
+
+import idom
+from idom.core.layout import Layout, RenderError, LayoutEvent
+from idom.core.render import SingleStateRenderer, SharedStateRenderer, StopRendering
+
+
+async def test_render_error_without_partial_render_raises():
+    class LayoutWithBadRenderError(Layout):
+        async def render(self):
+            raise RenderError("no partial render")
+
+    @idom.element
+    async def pass_element(self):
+        return idom.html.div()
+
+    bad_layout = LayoutWithBadRenderError(pass_element())
+    renderer = SingleStateRenderer(bad_layout)
+
+    async def send(data):
+        return None
+
+    async def recv():
+        while True:
+            await asyncio.sleep(1)
+
+    with pytest.raises(RenderError, match="no partial render"):
+        await renderer.run(send, recv, None)
+
+
+async def test_shared_state_renderer():
+    done = asyncio.Event()
+    data_sent_1 = asyncio.Queue()
+    data_sent_2 = []
+
+    async def send_1(data):
+        await data_sent_1.put(data)
+
+    async def recv_1():
+        sent = await data_sent_1.get()
+
+        element_id = sent["root"]
+        element_data = sent["new"][element_id]
+
+        if element_data["attributes"]["count"] == 4:
+            done.set()
+            raise StopRendering()
+
+        target = element_data["eventHandlers"]["anEvent"]["target"]
+        return LayoutEvent(target=target, data=[])
+
+    async def send_2(data):
+        element_id = data["root"]
+        element_data = data["new"][element_id]
+        data_sent_2.append(element_data["attributes"]["count"])
+        if done.is_set():
+            raise ValueError(data_sent_2)
+
+    async def recv_2():
+        await done.wait()
+        raise StopRendering()
+
+    @idom.element
+    async def Clickable(self, count=0):
+        @idom.event
+        async def an_event():
+            self.update(count=count + 1)
+
+        return idom.html.div({"anEvent": an_event, "count": count})
+
+    renderer = SharedStateRenderer(Layout(Clickable()))
+
+    await asyncio.gather(
+        renderer.run(send_1, recv_1, "1"), renderer.run(send_2, recv_2, "2"),
+    )
+
+    assert data_sent_2 == [0, 1, 2, 3, 4]
