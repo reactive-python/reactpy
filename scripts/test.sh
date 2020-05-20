b6d5324de4387999a57b19b5cbc0396fa2fbbbc1
rmorshea <ryan.morshead@gmail.com>
2020-05-19 20:28:00 -0700

    add spinner when installing js

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-pytest --headless -vv
-black . --check --exclude idom/client/node_modules/.*
+pytest --headless
+black . --check --exclude "idom/client/static/node_modules/.*"
 flake8 idom
 mypy --strict idom
 sphinx-build -b html docs/source docs/build
