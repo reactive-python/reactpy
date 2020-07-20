146eb96bc3fb22e5c6bae8df06a78da07773ddf6
rmorshea <ryan.morshead@gmail.com>
2020-07-19 19:42:50 -0700

    reintroduce concept of shared state to allow some imperative programming where needed

--- idom/core/hooks.py
+++ idom/core/hooks.py
@@ -1,9 +1,9 @@
 import time
 import asyncio
 from functools import lru_cache
-from typing import Dict, Any, TypeVar, Callable, Tuple, Optional, Type
+from typing import Dict, Any, TypeVar, Callable, Tuple, Optional, Type, Generic, Union
 
-from ._hooks import dispatch_hook, CreateContext, Context
+from ._hooks import current_hook, CreateContext, Context
 
 
 __all__ = [
@@ -19,14 +19,61 @@ __all__ = [
 
 
 def use_update() -> Callable[[], None]:
-    return dispatch_hook().use_update()
+    return current_hook().use_update()
 
 
 _StateType = TypeVar("_StateType")
 
 
-def use_state(value: _StateType) -> Tuple[_StateType, Callable[[_StateType], None]]:
-    hook = dispatch_hook()
+class Shared(Generic[_StateType]):
+
+    __slots__ = "_callbacks", "_value"
+
+    def __init__(self, value: _StateType) -> None:
+        self._callbacks: Dict[str, Callable[[_StateType], None]] = {}
+        self._value = value
+
+    def update(self, value: _StateType) -> None:
+        self._value = value
+        for cb in self._callbacks.values():
+            cb(value)
+
+
+def _new_is_not_old(old: _StateType, new: _StateType) -> bool:
+    return new is not old
+
+
+def use_state(
+    value: Union[_StateType, Shared[_StateType]],
+    should_update: Callable[[_StateType, _StateType], bool] = _new_is_not_old,
+) -> Tuple[_StateType, Callable[[_StateType], None]]:
+    if isinstance(value, Shared):
+        return _use_shared(value, should_update)
+    else:
+        return _use_state(value, should_update)
+
+
+def _use_shared(
+    shared: Shared[_StateType], should_update: Callable[[_StateType, _StateType], bool],
+) -> [_StateType, Callable[[_StateType], None]]:
+    hook = current_hook()
+    element_id = hook.element().id
+    update = hook.use_update()
+    hook.use_finalizer(shared._callbacks.pop, element_id, None)
+    shared._callbacks[element_id] = (
+        lambda value: update() if should_update(shared._value, value) else None
+    )
+
+    def set_state(new: _StateType) -> None:
+        shared.update(new)
+
+    return shared._value, set_state
+
+
+def _use_state(
+    value: _StateType, should_update: Callable[[_StateType, _StateType], bool],
+) -> Tuple[_StateType, Callable[[_StateType], None]]:
+    hook = current_hook()
     state: Dict[str, Any] = hook.use_state(dict)
     update = hook.use_update()
 
@@ -35,22 +82,18 @@ def use_state(value: _StateType) -> Tuple[_StateType, Callable[[_StateType], Non
 
     def set_state(new: _StateType) -> None:
         old = state["value"]
-        if new is not old:
+        if should_update(new, old):
             state["value"] = new
             update()
 
     return state["value"], set_state
 
 
-def _new_is_not_old(new: _StateType, old: _StateType) -> bool:
-    return new is not old
-
-
 def use_context(
     context_type: Type[Context[_StateType]],
     should_update: Callable[[_StateType, _StateType], bool] = _new_is_not_old,
 ) -> Tuple[_StateType, Callable[[_StateType], None]]:
-    return dispatch_hook().use_context(context_type, should_update)
+    return current_hook().use_context(context_type, should_update)
 
 
 _MemoValue = TypeVar("_MemoValue")
@@ -59,7 +102,7 @@ _MemoValue = TypeVar("_MemoValue")
 def use_memo(
     function: Callable[..., _MemoValue], *args: Any, **kwargs: Any
 ) -> _MemoValue:
-    hook = dispatch_hook()
+    hook = current_hook()
     cache: Dict[int, _MemoValue] = hook.use_state(dict)
 
     key = hash((args, tuple(kwargs.items())))
@@ -78,11 +121,11 @@ _LruFunc = TypeVar("_LruFunc")
 def use_lru_cache(
     function: _LruFunc, maxsize: Optional[int] = 128, typed: bool = False
 ) -> _LruFunc:
-    return dispatch_hook().use_state(lru_cache(maxsize, typed), function)
+    return current_hook().use_state(lru_cache(maxsize, typed), function)
 
 
 async def use_frame_rate(rate: float = 0) -> None:
-    await dispatch_hook().use_state(FramePacer, rate).wait()
+    await current_hook().use_state(FramePacer, rate).wait()
 
 
 class FramePacer:
