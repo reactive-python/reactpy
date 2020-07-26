9bb5b3c529e30ad98ffda0f0c2c0af91f44dc306
rmorshea <ryan.morshead@gmail.com>
2020-07-26 13:48:19 -0700

    refactor element state and life cycle management

--- idom/core/hooks.py
+++ idom/core/hooks.py
@@ -1,20 +1,27 @@
-import time
-import asyncio
 from functools import lru_cache
-from typing import Dict, Any, TypeVar, Callable, Tuple, Optional, Type, Generic, Union
+from threading import get_ident as get_thread_id
+from typing import (
+    Dict,
+    Any,
+    TypeVar,
+    Callable,
+    Tuple,
+    Optional,
+    Generic,
+    Union,
+    List,
+    TYPE_CHECKING,
+)
 
-from ._hooks import current_hook, CreateContext, Context
+if TYPE_CHECKING:
+    from .layout import ElementState
 
 
 __all__ = [
     "use_update",
     "use_state",
-    "use_context",
     "use_memo",
     "use_lru_cache",
-    "use_frame_rate",
-    "CreateContext",
-    "Context",
 ]
 
 
@@ -57,12 +64,11 @@ def _use_shared(
     shared: Shared[_StateType], should_update: Callable[[_StateType, _StateType], bool],
 ) -> [_StateType, Callable[[_StateType], None]]:
     hook = current_hook()
-    element_id = hook.element().id
     update = hook.use_update()
 
-    hook.use_finalizer(shared._callbacks.pop, element_id, None)
+    hook.use_finalizer(shared._callbacks.pop, hook.element_id, None)
     old = shared._value
-    shared._callbacks[element_id] = (
+    shared._callbacks[hook.element_id] = (
         lambda new: update() if should_update(new, old) else None
     )
 
@@ -89,13 +95,6 @@ def _use_state(
             update()
 
     return state["value"], set_state
-
-
-def use_context(
-    context_type: Type[Context[_StateType]],
-    should_update: Callable[[_StateType, _StateType], bool] = _new_is_not_old,
-) -> Tuple[_StateType, Callable[[_StateType], None]]:
-    return current_hook().use_context(context_type, should_update)
 
 
 _MemoValue = TypeVar("_MemoValue")
@@ -126,19 +125,82 @@ def use_lru_cache(
     return current_hook().use_state(lru_cache(maxsize, typed), function)
 
 
-async def use_frame_rate(rate: float = 0) -> None:
-    await current_hook().use_state(FramePacer, rate).wait()
+_current_life_cycle_hook: Dict[int, "LifeCycleHook"] = {}
 
 
-class FramePacer:
-    """Simple utility for pacing frames in an animation loop."""
+def current_hook() -> "LifeCycleHook":
+    try:
+        return _current_life_cycle_hook[get_thread_id()]
+    except KeyError as error:
+        msg = "No life cycle hook is active. Are you rendering in a layout?"
+        raise RuntimeError(msg) from error
 
-    __slots__ = "_rate", "_last"
 
-    def __init__(self, rate: float):
-        self._rate = rate
-        self._last = time.time()
+class LifeCycleHook:
 
-    async def wait(self) -> None:
-        await asyncio.sleep(self._rate - (time.time() - self._last))
-        self._last = time.time()
+    __slots__ = (
+        "_element_id",
+        "_element_state",
+        "_current_state_index",
+        "_state",
+        "_did_update",
+        "_has_rendered",
+        "_finalizers",
+    )
+
+    def __init__(self, element_state: "ElementState"):
+        self._element_state = element_state
+        self._element_id = element_state.element_id
+        self._current_state_index = 0
+        self._state: Tuple[Any, ...] = ()
+        self._finalizers: List[Callable[[], None]] = []
+        self._did_update = False
+        self._has_rendered = False
+
+    @property
+    def element_id(self):
+        return self._element_id
+
+    def use_update(self):
+        def update() -> None:
+            if not self._did_update:
+                self._did_update = True
+                self._element_state.update()
+            return None
+
+        return update
+
+    def use_state(
+        self, _function_: Callable[..., _StateType], *args: Any, **kwargs: Any
+    ) -> _StateType:
+        if not self._has_rendered:
+            # since we're not intialized yet we're just appending state
+            result = _function_(*args, **kwargs)
+            self._state += (result,)
+        else:
+            # once finalized we iterate over each succesively used piece of state
+            result = self._state[self._current_state_index]
+        self._current_state_index += 1
+        return result
+
+    def use_finalizer(
+        self, _function_: Callable[..., None], *args: Any, **kwargs: Any
+    ) -> None:
+        self._finalizers.append(lambda: _function_(*args, **kwargs))
+
+    def set_current(self):
+        _current_life_cycle_hook[get_thread_id()] = self
+
+    def unset_current(self):
+        # use an assert here for debug purposes since this should never be False
+        assert _current_life_cycle_hook.pop(get_thread_id()) is self
+
+    def element_did_render(self) -> None:
+        self._did_update = False
+        self._current_state_index = 0
+        self._has_rendered = True
+        self._finalizers.clear()
+
+    def element_will_unmount(self) -> None:
+        for func in self._finalizers:
+            func()
