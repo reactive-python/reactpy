138e75638170b579893cc029c28323b0706e1bef
rmorshea <ryan.morshead@gmail.com>
2021-01-26 00:02:44 -0800

    cov works on real install under nox

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,4 @@
 import os
-from typing import Iterable
 
 import nox
 from nox.sessions import Session
@@ -10,17 +9,14 @@ BLACK_DEFAULT_EXCLUDE = r"\.eggs|\.git|\.hg|\.mypy_cache|\.nox|\.tox|\.venv|\.sv
 
 
 @nox.session
-def test_without_coverage(session: Session) -> None:
-    _test_setup(session)
+def test_suite(session: Session) -> None:
+    session.env.update(os.environ)
+    session.install("-r", "requirements/test-env.txt")
     session.install(".[all]")
-    _test_run(session, extra_args=["--no-cov"])
-
-
-@nox.session
-def test_with_coverage(session: Session) -> None:
-    _test_setup(session)
-    session.install("-e", ".[all]")  # coverage requires a dev install
-    _test_run(session)
+    args = ["pytest", "tests"]
+    if HEADLESS:
+        args.append("--headless")
+    session.run(*args)
 
 
 @nox.session
@@ -49,16 +45,3 @@ def build_docs(session: Session) -> None:
     session.install("-e", ".[all]")
     session.run("sphinx-build", "-b", "html", "docs/source", "docs/build")
     session.run("sphinx-build", "-b", "doctest", "docs/source", "docs/build")
-
-
-def _test_setup(session: Session) -> None:
-    session.env.update(os.environ)
-    session.install("-r", "requirements/test-env.txt")
-
-
-def _test_run(session: Session, extra_args: Iterable[str] = ()) -> None:
-    args = ["pytest", "tests"]
-    if HEADLESS:
-        args.append("--headless")
-    args.extend(extra_args)
-    session.run(*args)
