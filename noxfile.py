93482800855171e28b391f3197c90dd2117e29ed
rmorshea <ryan.morshead@gmail.com>
2021-01-29 13:39:56 -0800

    upgrade pip first

--- noxfile.py
+++ noxfile.py
@@ -1,4 +1,6 @@
 import os
+from functools import wraps
+from typing import TypeVar
 
 import nox
 from nox.sessions import Session
@@ -8,7 +10,20 @@ HEADLESS = bool(int(os.environ.get("HEADLESS", "0")))
 BLACK_DEFAULT_EXCLUDE = r"\.eggs|\.git|\.hg|\.mypy_cache|\.nox|\.tox|\.venv|\.svn|_build|buck-out|build|dist"
 
 
+_Func = TypeVar("_Func")
+
+
+def upgrade_pip(func: _Func) -> _Func:
+    @wraps(func)
+    def decorator(session: Session) -> None:
+        session.install("--upgrade", "pip")
+        func(session)
+
+    return decorator
+
+
 @nox.session
+@upgrade_pip
 def test_python(session: Session) -> None:
     session.env.update(os.environ)
     session.install("-r", "requirements/test-env.txt")
@@ -20,6 +35,7 @@ def test_python(session: Session) -> None:
 
 
 @nox.session
+@upgrade_pip
 def check_types(session: Session) -> None:
     session.install("-r", "requirements/check-types.txt")
     session.install("-r", "requirements/pkg-deps.txt")
@@ -28,6 +44,7 @@ def check_types(session: Session) -> None:
 
 
 @nox.session
+@upgrade_pip
 def check_style(session: Session) -> None:
     session.install("-r", "requirements/check-style.txt")
     session.run(
@@ -41,7 +58,9 @@ def check_style(session: Session) -> None:
 
 
 @nox.session
+@upgrade_pip
 def build_docs(session: Session) -> None:
+    upgrade_pip(session)
     session.install("-r", "requirements/build-docs.txt")
     session.install("-e", ".[all]")
     session.run("sphinx-build", "-b", "html", "docs/source", "docs/build")
