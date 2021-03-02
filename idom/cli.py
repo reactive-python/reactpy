773f78ee1fcc8bf707b3937db2b951299134a138
rmorshea <ryan.morshead@gmail.com>
2021-03-02 12:31:13 -0800

    idom restore uses backup

--- idom/cli.py
+++ idom/cli.py
@@ -12,16 +12,19 @@ main.add_typer(show, name="show", short_help="Display information about IDOM")
 
 @main.command()
 def install(packages: List[str]) -> None:
+    """Install a Javascript package from NPM"""
     manage_client.build(packages)
     return None
 
 
 @main.command()
 def restore(clean_build: bool = True) -> None:
-    manage_client.build([], clean_build=clean_build)
+    """Return to a fresh install of IDOM's client"""
+    manage_client.restore()
     return None
 
 
 @show.command()
 def version() -> None:
+    """The version of IDOM"""
     typer.echo(idom.__version__)
