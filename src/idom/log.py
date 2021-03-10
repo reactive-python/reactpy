79059c96d02dedfee9b6cbafa80b718198b377df
rmorshea <ryan.morshead@gmail.com>
2021-03-10 01:54:20 -0800

    better logs for idom.run + flask server

--- src/idom/log.py
+++ src/idom/log.py
@@ -25,7 +25,7 @@ LOGGING_CONFIG_DEFAULTS = {
     },
     "formatters": {
         "generic": {
-            "format": "%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+            "format": "%(asctime)s | %(levelname)s | %(message)s",
             "datefmt": r"%Y-%m-%dT%H:%M:%S%z",
             "class": "logging.Formatter",
         }
