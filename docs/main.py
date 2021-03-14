5dfef51409d446010202f26423118054db72990e
rmorshea <ryan.morshead@gmail.com>
2021-03-14 00:12:32 -0800

    accidentally set serve static files to false

--- docs/main.py
+++ docs/main.py
@@ -52,7 +52,7 @@ finally:
 
 
 PerClientStateServer(
-    component, {"serve_static_files": False, "url_prefix": "/_idom"}
+    component, {"redirect_root_to_index": False, "url_prefix": "/_idom"}
 ).register(app)
 
 
