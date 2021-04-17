42ee01c1f36455a8a63b601938a23834c3d70aa2
rmorshea <ryan.morshead@gmail.com>
2021-04-17 13:50:32 -0700

    add feature flag for default key behavior

--- src/idom/config.py
+++ src/idom/config.py
@@ -52,3 +52,29 @@ Examples:
     Absolute URL
         ``ABSOLUTE_PATH/my-module.js``
 """
+
+IDOM_FEATURE_INDEX_AS_DEFAULT_KEY = _option.Option(
+    "IDOM_FEATURE_INDEX_AS_DEFAULT_KEY",
+    default=False,
+    mutable=False,
+    validator=lambda x: bool(int(x)),
+)
+"""A feature flag for using the index of a sibling element as its default key
+
+In a future release this flag's default value will be set to true, and after that, this
+flag willbe removed entirely and the indices will always be the default key.
+
+For more information on changes to this feature flag see: https://github.com/idom-team/idom/issues/351
+"""
+
+if not IDOM_FEATURE_INDEX_AS_DEFAULT_KEY.get():  # pragma: no cover
+    from warnings import warn
+
+    warn(
+        (
+            "In a future release 'IDOM_FEATURE_INDEX_AS_DEFAULT_KEY' will be turned on "
+            "by default. For more information on changes to this feature flag, see: "
+            "https://github.com/idom-team/idom/issues/351"
+        ),
+        UserWarning,
+    )
