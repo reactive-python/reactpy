aabfbd2ea4e07e26be228c3655cd74bcb6f1cc98
rmorshea <ryan.morshead@gmail.com>
2021-05-07 21:37:43 -0700

    delete accidentally added files

--- asd/README.md
+++
@@ -1,3 +0,0 @@
-# IDOM Client
-
-An ES6 Javascript client for IDOM
--- asd/_snowpack/pkg/htm.js
+++
@@ -1,2 +0,0 @@
-import { h as htm } from './common/htm.module-dd7abb54.js';
-export { h as default } from './common/htm.module-dd7abb54.js';
