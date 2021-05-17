0cc03babeac3d20c74fb6baa6e86d16a9f4148cd
rmorshea <ryan.morshead@gmail.com>
2021-05-16 19:41:23 -0700

    refactor fastapi using server protocol

--- src/idom/server/fastapi.py
+++ src/idom/server/fastapi.py
@@ -2,13 +2,15 @@
 FastAPI Servers
 ===============
 """
+from __future__ import annotations
 
 import asyncio
 import json
 import logging
 import sys
 import time
-from threading import Event, Thread
+from asyncio import Future
+from threading import Event, Thread, current_thread
 from typing import Any, Dict, Optional, Tuple, Union
 
 from fastapi import APIRouter, FastAPI, Request, WebSocket
@@ -23,15 +25,15 @@ from uvicorn.supervisors.multiprocess import Multiprocess
 from uvicorn.supervisors.statreload import StatReload as ChangeReload
 
 from idom.config import IDOM_CLIENT_BUILD_DIR
+from idom.core.component import ComponentConstructor
 from idom.core.dispatcher import (
     RecvCoroutine,
     SendCoroutine,
+    SharedViewDispatcher,
     dispatch_single_view,
     ensure_shared_view_dispatcher_future,
 )
 from idom.core.layout import Layout, LayoutEvent, LayoutUpdate
-
-from .base import AbstractRenderServer
 
 
 logger = logging.getLogger(__name__)
@@ -46,209 +48,206 @@ class Config(TypedDict, total=False):
     redirect_root_to_index: bool
 
 
-class FastApiRenderServer(AbstractRenderServer[FastAPI, Config]):
-    """Base ``sanic`` extension."""
+def PerClientStateServer(
+    constructor: ComponentConstructor,
+    config: Optional[Config] = None,
+    app: Optional[FastAPI] = None,
+) -> FastApiServer:
+    config, app = _setup_config_and_app(config, app)
+    router = APIRouter(prefix=config["url_prefix"])
+    _setup_common_routes(app, router, config)
+    _setup_single_view_dispatcher_route(router, constructor)
+    app.include_router(router)
+    return FastApiServer(app)
 
+
+def SharedClientStateServer(
+    constructor: ComponentConstructor,
+    config: Optional[Config] = None,
+    app: Optional[FastAPI] = None,
+) -> FastApiServer:
+    config, app = _setup_config_and_app(config, app)
+    router = APIRouter(prefix=config["url_prefix"])
+    _setup_common_routes(app, router, config)
+    _setup_shared_view_dispatcher_route(app, router, constructor)
+    app.include_router(router)
+    return FastApiServer(app)
+
+
+class FastApiServer:
     _server: UvicornServer
+    _current_thread: Thread
+
+    def __init__(self, app: FastAPI) -> None:
+        self.app = app
+        self._did_stop = Event()
+        app.on_event("shutdown")(self._server_did_stop)
+
+    def run(self, host: str, port: int, *args: Any, **kwargs: Any) -> None:
+        self._current_thread = current_thread()
+
+        self._server = server = UvicornServer(
+            UvicornConfig(
+                self.app, host=host, port=port, loop="asyncio", *args, **kwargs
+            )
+        )
+
+        # The following was copied from the uvicorn source with minimal modification. We
+        # shouldn't need to do this, but unfortunately there's no easy way to gain access to
+        # the server instance so you can stop it.
+        # BUG: https://github.com/encode/uvicorn/issues/742
+        config = server.config
+
+        if (config.reload or config.workers > 1) and not isinstance(
+            server.config.app, str
+        ):  # pragma: no cover
+            logger = logging.getLogger("uvicorn.error")
+            logger.warning(
+                "You must pass the application as an import string to enable 'reload' or "
+                "'workers'."
+            )
+            sys.exit(1)
+
+        if config.should_reload:  # pragma: no cover
+            sock = config.bind_socket()
+            supervisor = ChangeReload(config, target=server.run, sockets=[sock])
+            supervisor.run()
+        elif config.workers > 1:  # pragma: no cover
+            sock = config.bind_socket()
+            supervisor = Multiprocess(config, target=server.run, sockets=[sock])
+            supervisor.run()
+        else:
+            import asyncio
+
+            asyncio.set_event_loop(asyncio.new_event_loop())
+            server.run()
+
+    def run_in_thread(self, host: str, port: int, *args: Any, **kwargs: Any) -> None:
+        thread = Thread(
+            target=lambda: self.run(host, port, *args, *kwargs), daemon=True
+        )
+        thread.start()
+
+    def wait_until_started(self, timeout: Optional[float] = 3.0) -> None:
+        while self._current_thread.is_alive() and (
+            not hasattr(self, "_server") or not self._server.started
+        ):
+            time.sleep(0.01)
 
     def stop(self, timeout: Optional[float] = 3.0) -> None:
-        """Stop the running application"""
-        self._server.should_exit
-        if self._daemon_thread is not None:
-            self._daemon_thread.join(timeout)
+        self._server.should_exit = True
+        self._did_stop.wait(timeout)
 
-    def _create_config(self, config: Optional[Config]) -> Config:
-        new_config: Config = {
+    async def _server_did_stop(self) -> None:
+        self._did_stop.set()
+
+
+def _setup_config_and_app(
+    config: Optional[Config],
+    app: Optional[FastAPI],
+) -> Tuple[Config, FastAPI]:
+    return (
+        {
             "cors": False,
             "url_prefix": "",
             "serve_static_files": True,
             "redirect_root_to_index": True,
             **(config or {}),  # type: ignore
-        }
-        return new_config
+        },
+        app or FastAPI(),
+    )
 
-    def _default_application(self, config: Config) -> FastAPI:
-        return FastAPI()
 
-    def _setup_application(self, config: Config, app: FastAPI) -> None:
-        router = APIRouter(prefix=config["url_prefix"])
+def _setup_common_routes(app: FastAPI, router: APIRouter, config: Config) -> None:
+    cors_config = config["cors"]
+    if cors_config:  # pragma: no cover
+        cors_params = (
+            cors_config if isinstance(cors_config, dict) else {"allow_origins": ["*"]}
+        )
+        app.add_middleware(CORSMiddleware, **cors_params)
 
-        self._setup_api_router(config, router)
-        self._setup_static_files(config, app)
+    # This really should be added to the APIRouter, but there's a bug in FastAPI
+    # BUG: https://github.com/tiangolo/fastapi/issues/1469
+    url_prefix = config["url_prefix"]
+    if config["serve_static_files"]:
+        app.mount(
+            f"{url_prefix}/client",
+            StaticFiles(
+                directory=str(IDOM_CLIENT_BUILD_DIR.current),
+                html=True,
+                check_dir=True,
+            ),
+            name="idom_static_files",
+        )
 
-        cors_config = config["cors"]
-        if cors_config:  # pragma: no cover
-            cors_params = (
-                cors_config
-                if isinstance(cors_config, dict)
-                else {"allow_origins": ["*"]}
-            )
-            app.add_middleware(CORSMiddleware, **cors_params)
+        if config["redirect_root_to_index"]:
 
-        app.include_router(router)
-
-    def _setup_application_did_start_event(
-        self, config: Config, app: FastAPI, event: Event
-    ) -> None:
-        def target() -> None:
-            while not hasattr(self, "_server") or not self._server.started:
-                time.sleep(1e-3)
-            event.set()
-
-        Thread(target=target, daemon=True).start()
-
-    def _setup_api_router(self, config: Config, router: APIRouter) -> None:
-        """Add routes to the application blueprint"""
-
-        @router.websocket("/stream")
-        async def model_stream(socket: WebSocket) -> None:
-            await socket.accept()
-
-            async def sock_send(value: LayoutUpdate) -> None:
-                await socket.send_text(json.dumps(value))
-
-            async def sock_recv() -> LayoutEvent:
-                return LayoutEvent(**json.loads(await socket.receive_text()))
-
-            try:
-                await self._run_dispatcher(
-                    sock_send, sock_recv, dict(socket.query_params)
+            @app.route(f"{url_prefix}/")
+            def redirect_to_index(request: Request) -> RedirectResponse:
+                return RedirectResponse(
+                    f"{url_prefix}/client/index.html?{request.query_params}"
                 )
-            except WebSocketDisconnect as error:
-                logger.info(f"WebSocket disconnect: {error.code}")
 
-    def _setup_static_files(self, config: Config, app: FastAPI) -> None:
-        # This really should be added to the APIRouter, but there's a bug in FastAPI
-        # BUG: https://github.com/tiangolo/fastapi/issues/1469
-        url_prefix = config["url_prefix"]
-        if config["serve_static_files"]:
-            app.mount(
-                f"{url_prefix}/client",
-                StaticFiles(
-                    directory=str(IDOM_CLIENT_BUILD_DIR.current),
-                    html=True,
-                    check_dir=True,
-                ),
-                name="idom_static_files",
+
+def _setup_single_view_dispatcher_route(
+    router: APIRouter, constructor: ComponentConstructor
+) -> None:
+    @router.websocket("/stream")
+    async def model_stream(socket: WebSocket) -> None:
+        await socket.accept()
+        send, recv = _make_send_recv_callbacks(socket)
+        try:
+            await dispatch_single_view(
+                Layout(constructor(**dict(socket.query_params))), send, recv
+            )
+        except WebSocketDisconnect as error:
+            logger.info(f"WebSocket disconnect: {error.code}")
+
+
+def _setup_shared_view_dispatcher_route(
+    app: FastAPI, router: APIRouter, constructor: ComponentConstructor
+) -> None:
+    dispatcher_future: Future[None]
+    dispatch_coroutine: SharedViewDispatcher
+
+    @app.on_event("startup")
+    async def activate_dispatcher() -> None:
+        nonlocal dispatcher_future
+        nonlocal dispatch_coroutine
+        dispatcher_future, dispatch_coroutine = ensure_shared_view_dispatcher_future(
+            Layout(constructor())
+        )
+
+    @app.on_event("shutdown")
+    async def deactivate_dispatcher() -> None:
+        logger.debug("Stopping dispatcher - server is shutting down")
+        dispatcher_future.cancel()
+        await asyncio.wait([dispatcher_future])
+
+    @router.websocket("/stream")
+    async def model_stream(socket: WebSocket) -> None:
+        await socket.accept()
+
+        if socket.query_params:
+            raise ValueError(
+                "SharedClientState server does not support per-client view parameters"
             )
 
-            if config["redirect_root_to_index"]:
+        send, recv = _make_send_recv_callbacks(socket)
 
-                @app.route(f"{url_prefix}/")
-                def redirect_to_index(request: Request) -> RedirectResponse:
-                    return RedirectResponse(
-                        f"{url_prefix}/client/index.html?{request.query_params}"
-                    )
-
-    def _run_application(
-        self,
-        config: Config,
-        app: FastAPI,
-        host: str,
-        port: int,
-        args: Tuple[Any, ...],
-        kwargs: Dict[str, Any],
-    ) -> None:
-        self._server = UvicornServer(
-            UvicornConfig(app, host=host, port=port, loop="asyncio", *args, **kwargs)
-        )
-        _run_uvicorn_server(self._server)
-
-    def _run_application_in_thread(
-        self,
-        config: Config,
-        app: FastAPI,
-        host: str,
-        port: int,
-        args: Tuple[Any, ...],
-        kwargs: Dict[str, Any],
-    ) -> None:
-        # uvicorn does the event loop setup for us
-        self._run_application(config, app, host, port, args, kwargs)
-
-    async def _run_dispatcher(
-        self, send: SendCoroutine, recv: RecvCoroutine, params: Dict[str, Any]
-    ) -> None:
-        raise NotImplementedError()
+        try:
+            await dispatch_coroutine(send, recv)
+        except WebSocketDisconnect as error:
+            logger.info(f"WebSocket disconnect: {error.code}")
 
 
-class PerClientStateServer(FastApiRenderServer):
-    """Each client view will have its own state."""
+def _make_send_recv_callbacks(
+    socket: WebSocket,
+) -> Tuple[SendCoroutine, RecvCoroutine]:
+    async def sock_send(value: LayoutUpdate) -> None:
+        await socket.send_text(json.dumps(value))
 
-    async def _run_dispatcher(
-        self,
-        send: SendCoroutine,
-        recv: RecvCoroutine,
-        params: Dict[str, Any],
-    ) -> None:
-        await dispatch_single_view(
-            Layout(self._root_component_constructor(**params)),
-            send,
-            recv,
-        )
+    async def sock_recv() -> LayoutEvent:
+        return LayoutEvent(**json.loads(await socket.receive_text()))
 
-
-class SharedClientStateServer(FastApiRenderServer):
-    """All connected client views will have shared state."""
-
-    def _setup_application(self, config: Config, app: FastAPI) -> None:
-        app.on_event("startup")(self._activate_dispatcher)
-        app.on_event("shutdown")(self._deactivate_dispatcher)
-        super()._setup_application(config, app)
-
-    async def _activate_dispatcher(self) -> None:
-        (
-            self._dispatch_daemon_future,
-            self._dispatch_coroutine,
-        ) = ensure_shared_view_dispatcher_future(
-            Layout(self._root_component_constructor())
-        )
-
-    async def _deactivate_dispatcher(self) -> None:  # pragma: no cover
-        # for some reason this isn't getting run during testing
-        logger.debug("Stopping dispatcher - server is shutting down")
-        self._dispatch_daemon_future.cancel()
-        await asyncio.wait([self._dispatch_daemon_future])
-
-    async def _run_dispatcher(
-        self,
-        send: SendCoroutine,
-        recv: RecvCoroutine,
-        params: Dict[str, Any],
-    ) -> None:
-        if params:
-            msg = f"SharedClientState server does not support per-client view parameters {params}"
-            raise ValueError(msg)
-        await self._dispatch_coroutine(send, recv)
-
-
-def _run_uvicorn_server(server: UvicornServer) -> None:
-    # The following was copied from the uvicorn source with minimal modification. We
-    # shouldn't need to do this, but unfortunately there's no easy way to gain access to
-    # the server instance so you can stop it.
-    # BUG: https://github.com/encode/uvicorn/issues/742
-    config = server.config
-
-    if (config.reload or config.workers > 1) and not isinstance(
-        server.config.app, str
-    ):  # pragma: no cover
-        logger = logging.getLogger("uvicorn.error")
-        logger.warning(
-            "You must pass the application as an import string to enable 'reload' or "
-            "'workers'."
-        )
-        sys.exit(1)
-
-    if config.should_reload:  # pragma: no cover
-        sock = config.bind_socket()
-        supervisor = ChangeReload(config, target=server.run, sockets=[sock])
-        supervisor.run()
-    elif config.workers > 1:  # pragma: no cover
-        sock = config.bind_socket()
-        supervisor = Multiprocess(config, target=server.run, sockets=[sock])
-        supervisor.run()
-    else:
-        import asyncio
-
-        asyncio.set_event_loop(asyncio.new_event_loop())
-        server.run()
+    return sock_send, sock_recv
