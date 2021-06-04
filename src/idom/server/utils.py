eac2951ad9a34821c75b77ad429bf508d4d91246
rmorshea <ryan.morshead@gmail.com>
2021-06-03 23:56:19 -0700

    fix find port func
    need to set SO_REUSEADDR because socket is in STATE_WAIT
    as per this diagram:
    https://en.wikipedia.org/wiki/File%3aTcp_state_diagram_fixed_new.svg

--- src/idom/server/utils.py
+++ src/idom/server/utils.py
@@ -1,9 +1,10 @@
 import asyncio
+import errno
+import socket
 import time
 from contextlib import closing
 from functools import wraps
 from importlib import import_module
-from socket import socket
 from threading import Event, Thread
 from typing import Any, Callable, List, Optional, TypeVar, cast
 
@@ -86,13 +87,15 @@ def find_builtin_server_type(type_name: str) -> ServerFactory[Any, Any]:
 def find_available_port(host: str, port_min: int = 8000, port_max: int = 9000) -> int:
     """Get a port that's available for the given host and port range"""
     for port in range(port_min, port_max):
-        with closing(socket()) as sock:
+        with closing(socket.socket()) as sock:
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             try:
                 sock.bind((host, port))
-            except OSError:
-                pass
+            except socket.error as error:
+                if error.errno != errno.EADDRINUSE:
+                    raise
             else:
                 return port
     raise RuntimeError(
-        f"Host {host!r} has no available port in range {port_max}-{port_max}"
+        f"Host {host!r} has no available port in range {port_min}-{port_max}"
     )
