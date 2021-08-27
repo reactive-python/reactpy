384bd934ccefc35d492ac02ce1149b58ff45c742
rmorshea <ryan.morshead@gmail.com>
2021-08-27 01:10:35 -0700

    update docs custom js from last change

--- docs/source/_static/custom.js
+++ docs/source/_static/custom.js
@@ -620,187 +620,6 @@ function checkDCE() {
 
 var n=function(t,s,r,e){var u;s[0]=0;for(var h=1;h<s.length;h++){var p=s[h++],a=s[h]?(s[0]|=p?1:2,r[s[h++]]):s[++h];3===p?e[0]=a:4===p?e[1]=Object.assign(e[1]||{},a):5===p?(e[1]=e[1]||{})[s[++h]]=a:6===p?e[1][s[++h]]+=a+"":p?(u=t.apply(a,n(t,a,r,["",null])),e.push(u),a[0]?s[0]|=2:(s[h-2]=0,s[h]=u)):e.push(a);}return e},t=new Map;function htm(s){var r=t.get(this);return r||(r=new Map,t.set(this,r)),(r=n(this,r.get(s)||(r.set(s,r=function(n){for(var t,s,r=1,e="",u="",h=[0],p=function(n){1===r&&(n||(e=e.replace(/^\s*\n\s*|\s*\n\s*$/g,"")))?h.push(0,n,e):3===r&&(n||e)?(h.push(3,n,e),r=2):2===r&&"..."===e&&n?h.push(4,n,0):2===r&&e&&!n?h.push(5,0,!0,e):r>=5&&((e||!n&&5===r)&&(h.push(r,0,e,s),r=6),n&&(h.push(r,n,0,s),r=6)),e="";},a=0;a<n.length;a++){a&&(1===r&&p(),p(a));for(var l=0;l<n[a].length;l++)t=n[a][l],1===r?"<"===t?(p(),h=[h],r=3):e+=t:4===r?"--"===e&&">"===t?(r=1,e=""):e=t+e[0]:u?t===u?u="":e+=t:'"'===t||"'"===t?u=t:">"===t?(p(),r=1):r&&("="===t?(r=5,s=e,e=""):"/"===t&&(r<5||">"===n[a][l+1])?(p(),3===r&&(h=h[0]),r=h,(h=h[0]).push(2,0,r),r=0):" "===t||"\t"===t||"\n"===t||"\r"===t?(p(),r=2):e+=t),3===r&&"!--"===e&&(r=4,h=h[0]);}return p(),h}(s)),r),arguments,[])).length>1?r:r[0]}
 
-function serializeEvent(event) {
-  const data = {};
-
-  if (event.type in eventTransforms) {
-    Object.assign(data, eventTransforms[event.type](event));
-  }
-
-  const target = event.target;
-  if (target.tagName in targetTransforms) {
-    targetTransforms[target.tagName].forEach((trans) =>
-      Object.assign(data, trans(target))
-    );
-  }
-
-  return data;
-}
-
-const targetTransformCategories = {
-  hasValue: (target) => ({
-    value: target.value,
-  }),
-  hasCurrentTime: (target) => ({
-    currentTime: target.currentTime,
-  }),
-  hasFiles: (target) => {
-    if (target?.type == "file") {
-      return {
-        files: Array.from(target.files).map((file) => ({
-          lastModified: file.lastModified,
-          name: file.name,
-          size: file.size,
-          type: file.type,
-        })),
-      };
-    } else {
-      return {};
-    }
-  },
-};
-
-const targetTagCategories = {
-  hasValue: ["BUTTON", "INPUT", "OPTION", "LI", "METER", "PROGRESS", "PARAM"],
-  hasCurrentTime: ["AUDIO", "VIDEO"],
-  hasFiles: ["INPUT"],
-};
-
-const targetTransforms = {};
-
-Object.keys(targetTagCategories).forEach((category) => {
-  targetTagCategories[category].forEach((type) => {
-    const transforms = targetTransforms[type] || (targetTransforms[type] = []);
-    transforms.push(targetTransformCategories[category]);
-  });
-});
-
-const eventTransformCategories = {
-  clipboard: (event) => ({
-    clipboardData: event.clipboardData,
-  }),
-  composition: (event) => ({
-    data: event.data,
-  }),
-  keyboard: (event) => ({
-    altKey: event.altKey,
-    charCode: event.charCode,
-    ctrlKey: event.ctrlKey,
-    key: event.key,
-    keyCode: event.keyCode,
-    locale: event.locale,
-    location: event.location,
-    metaKey: event.metaKey,
-    repeat: event.repeat,
-    shiftKey: event.shiftKey,
-    which: event.which,
-  }),
-  mouse: (event) => ({
-    altKey: event.altKey,
-    button: event.button,
-    buttons: event.buttons,
-    clientX: event.clientX,
-    clientY: event.clientY,
-    ctrlKey: event.ctrlKey,
-    metaKey: event.metaKey,
-    pageX: event.pageX,
-    pageY: event.pageY,
-    screenX: event.screenX,
-    screenY: event.screenY,
-    shiftKey: event.shiftKey,
-  }),
-  pointer: (event) => ({
-    pointerId: event.pointerId,
-    width: event.width,
-    height: event.height,
-    pressure: event.pressure,
-    tiltX: event.tiltX,
-    tiltY: event.tiltY,
-    pointerType: event.pointerType,
-    isPrimary: event.isPrimary,
-  }),
-  selection: () => {
-    return { selectedText: window.getSelection().toString() };
-  },
-  touch: (event) => ({
-    altKey: event.altKey,
-    ctrlKey: event.ctrlKey,
-    metaKey: event.metaKey,
-    shiftKey: event.shiftKey,
-  }),
-  ui: (event) => ({
-    detail: event.detail,
-  }),
-  wheel: (event) => ({
-    deltaMode: event.deltaMode,
-    deltaX: event.deltaX,
-    deltaY: event.deltaY,
-    deltaZ: event.deltaZ,
-  }),
-  animation: (event) => ({
-    animationName: event.animationName,
-    pseudoElement: event.pseudoElement,
-    elapsedTime: event.elapsedTime,
-  }),
-  transition: (event) => ({
-    propertyName: event.propertyName,
-    pseudoElement: event.pseudoElement,
-    elapsedTime: event.elapsedTime,
-  }),
-};
-
-const eventTypeCategories = {
-  clipboard: ["copy", "cut", "paste"],
-  composition: ["compositionend", "compositionstart", "compositionupdate"],
-  keyboard: ["keydown", "keypress", "keyup"],
-  mouse: [
-    "click",
-    "contextmenu",
-    "doubleclick",
-    "drag",
-    "dragend",
-    "dragenter",
-    "dragexit",
-    "dragleave",
-    "dragover",
-    "dragstart",
-    "drop",
-    "mousedown",
-    "mouseenter",
-    "mouseleave",
-    "mousemove",
-    "mouseout",
-    "mouseover",
-    "mouseup",
-  ],
-  pointer: [
-    "pointerdown",
-    "pointermove",
-    "pointerup",
-    "pointercancel",
-    "gotpointercapture",
-    "lostpointercapture",
-    "pointerenter",
-    "pointerleave",
-    "pointerover",
-    "pointerout",
-  ],
-  selection: ["select"],
-  touch: ["touchcancel", "touchend", "touchmove", "touchstart"],
-  ui: ["scroll"],
-  wheel: ["wheel"],
-  animation: ["animationstart", "animationend", "animationiteration"],
-  transition: ["transitionend"],
-};
-
-const eventTransforms = {};
-
-Object.keys(eventTypeCategories).forEach((category) => {
-  eventTypeCategories[category].forEach((type) => {
-    eventTransforms[type] = eventTransformCategories[category];
-  });
-});
-
 /*!
  * https://github.com/Starcounter-Jack/JSON-Patch
  * (c) 2017 Joachim Wester
@@ -1569,14 +1388,186 @@ var jsonpatch = Object.assign({}, core, duplex, {
     unescapePathComponent
 });
 
-function applyPatchInplace(doc, pathPrefix, patch) {
-  if (pathPrefix) {
-    patch = patch.map((op) =>
-      Object.assign({}, op, { path: pathPrefix + op.path })
+function serializeEvent(event) {
+  const data = {};
+
+  if (event.type in eventTransforms) {
+    Object.assign(data, eventTransforms[event.type](event));
+  }
+
+  const target = event.target;
+  if (target.tagName in targetTransforms) {
+    targetTransforms[target.tagName].forEach((trans) =>
+      Object.assign(data, trans(target))
     );
   }
-  jsonpatch.applyPatch(doc, patch, false, true);
+
+  return data;
 }
+
+const targetTransformCategories = {
+  hasValue: (target) => ({
+    value: target.value,
+  }),
+  hasCurrentTime: (target) => ({
+    currentTime: target.currentTime,
+  }),
+  hasFiles: (target) => {
+    if (target?.type == "file") {
+      return {
+        files: Array.from(target.files).map((file) => ({
+          lastModified: file.lastModified,
+          name: file.name,
+          size: file.size,
+          type: file.type,
+        })),
+      };
+    } else {
+      return {};
+    }
+  },
+};
+
+const targetTagCategories = {
+  hasValue: ["BUTTON", "INPUT", "OPTION", "LI", "METER", "PROGRESS", "PARAM"],
+  hasCurrentTime: ["AUDIO", "VIDEO"],
+  hasFiles: ["INPUT"],
+};
+
+const targetTransforms = {};
+
+Object.keys(targetTagCategories).forEach((category) => {
+  targetTagCategories[category].forEach((type) => {
+    const transforms = targetTransforms[type] || (targetTransforms[type] = []);
+    transforms.push(targetTransformCategories[category]);
+  });
+});
+
+const eventTransformCategories = {
+  clipboard: (event) => ({
+    clipboardData: event.clipboardData,
+  }),
+  composition: (event) => ({
+    data: event.data,
+  }),
+  keyboard: (event) => ({
+    altKey: event.altKey,
+    charCode: event.charCode,
+    ctrlKey: event.ctrlKey,
+    key: event.key,
+    keyCode: event.keyCode,
+    locale: event.locale,
+    location: event.location,
+    metaKey: event.metaKey,
+    repeat: event.repeat,
+    shiftKey: event.shiftKey,
+    which: event.which,
+  }),
+  mouse: (event) => ({
+    altKey: event.altKey,
+    button: event.button,
+    buttons: event.buttons,
+    clientX: event.clientX,
+    clientY: event.clientY,
+    ctrlKey: event.ctrlKey,
+    metaKey: event.metaKey,
+    pageX: event.pageX,
+    pageY: event.pageY,
+    screenX: event.screenX,
+    screenY: event.screenY,
+    shiftKey: event.shiftKey,
+  }),
+  pointer: (event) => ({
+    pointerId: event.pointerId,
+    width: event.width,
+    height: event.height,
+    pressure: event.pressure,
+    tiltX: event.tiltX,
+    tiltY: event.tiltY,
+    pointerType: event.pointerType,
+    isPrimary: event.isPrimary,
+  }),
+  selection: () => {
+    return { selectedText: window.getSelection().toString() };
+  },
+  touch: (event) => ({
+    altKey: event.altKey,
+    ctrlKey: event.ctrlKey,
+    metaKey: event.metaKey,
+    shiftKey: event.shiftKey,
+  }),
+  ui: (event) => ({
+    detail: event.detail,
+  }),
+  wheel: (event) => ({
+    deltaMode: event.deltaMode,
+    deltaX: event.deltaX,
+    deltaY: event.deltaY,
+    deltaZ: event.deltaZ,
+  }),
+  animation: (event) => ({
+    animationName: event.animationName,
+    pseudoElement: event.pseudoElement,
+    elapsedTime: event.elapsedTime,
+  }),
+  transition: (event) => ({
+    propertyName: event.propertyName,
+    pseudoElement: event.pseudoElement,
+    elapsedTime: event.elapsedTime,
+  }),
+};
+
+const eventTypeCategories = {
+  clipboard: ["copy", "cut", "paste"],
+  composition: ["compositionend", "compositionstart", "compositionupdate"],
+  keyboard: ["keydown", "keypress", "keyup"],
+  mouse: [
+    "click",
+    "contextmenu",
+    "doubleclick",
+    "drag",
+    "dragend",
+    "dragenter",
+    "dragexit",
+    "dragleave",
+    "dragover",
+    "dragstart",
+    "drop",
+    "mousedown",
+    "mouseenter",
+    "mouseleave",
+    "mousemove",
+    "mouseout",
+    "mouseover",
+    "mouseup",
+  ],
+  pointer: [
+    "pointerdown",
+    "pointermove",
+    "pointerup",
+    "pointercancel",
+    "gotpointercapture",
+    "lostpointercapture",
+    "pointerenter",
+    "pointerleave",
+    "pointerover",
+    "pointerout",
+  ],
+  selection: ["select"],
+  touch: ["touchcancel", "touchend", "touchmove", "touchstart"],
+  ui: ["scroll"],
+  wheel: ["wheel"],
+  animation: ["animationstart", "animationend", "animationiteration"],
+  transition: ["transitionend"],
+};
+
+const eventTransforms = {};
+
+Object.keys(eventTypeCategories).forEach((category) => {
+  eventTypeCategories[category].forEach((type) => {
+    eventTransforms[type] = eventTransformCategories[category];
+  });
+});
 
 const html = htm.bind(react.createElement);
 const LayoutConfigContext = react.createContext({
@@ -1585,7 +1576,7 @@ const LayoutConfigContext = react.createContext({
 });
 
 function Layout({ saveUpdateHook, sendEvent, loadImportSource }) {
-  const [model, patchModel] = useInplaceJsonPatch({});
+  const [model, patchModel] = useJsonPatchCallback({});
 
   react.useEffect(() => saveUpdateHook(patchModel), [patchModel]);
 
@@ -1754,19 +1745,32 @@ function loadImportSource$1(config, importSource) {
     });
 }
 
-function useInplaceJsonPatch(doc) {
-  const ref = react.useRef(doc);
+function useJsonPatchCallback(initial) {
+  const model = react.useRef(initial);
   const forceUpdate = useForceUpdate();
 
   const applyPatch = react.useCallback(
-    (path, patch) => {
-      applyPatchInplace(ref.current, path, patch);
+    (pathPrefix, patch) => {
+      if (pathPrefix) {
+        patch = patch.map((op) =>
+          Object.assign({}, op, { path: pathPrefix + op.path })
+        );
+      }
+      // Always return a newDocument because React checks some attributes of the model
+      // (e.g. model.attributes.style is checked for identity)
+      model.current = jsonpatch.applyPatch(
+        model.current,
+        patch,
+        false,
+        false,
+        true
+      ).newDocument;
       forceUpdate();
     },
-    [ref, forceUpdate]
+    [model]
   );
 
-  return [ref.current, applyPatch];
+  return [model.current, applyPatch];
 }
 
 function useForceUpdate() {
