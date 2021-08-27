86309d070cab6912b5a1098e832139b6284a7ac1
rmorshea <ryan.morshead@gmail.com>
2021-08-27 01:36:43 -0700

    fix model mutation in client
    The client-side model was being mutated on each update
    in order to try and be more performant. This is a problem
    though because React relies on checking the identity of
    some parts of the model to see if things have changed.
    
    No we create a copy of the model each time it changed

--- src/client/packages/idom-client-react/src/utils.js
+++ src/client/packages/idom-client-react/src/utils.js
@@ -1,16 +1,35 @@
+import React from "react";
 import jsonpatch from "fast-json-patch";
 
-export function applyPatchInplace(doc, pathPrefix, patch) {
-  if (pathPrefix) {
-    patch = patch.map((op) =>
-      Object.assign({}, op, { path: pathPrefix + op.path })
-    );
-  }
-  jsonpatch.applyPatch(doc, patch, false, true);
+export function useJsonPatchCallback(initial) {
+  const model = React.useRef(initial);
+  const forceUpdate = useForceUpdate();
+
+  const applyPatch = React.useCallback(
+    (pathPrefix, patch) => {
+      if (pathPrefix) {
+        patch = patch.map((op) =>
+          Object.assign({}, op, { path: pathPrefix + op.path })
+        );
+      }
+      // Always return a newDocument because React checks some attributes of the model
+      // (e.g. model.attributes.style is checked for identity)
+      model.current = jsonpatch.applyPatch(
+        model.current,
+        patch,
+        false,
+        false,
+        true
+      ).newDocument;
+      forceUpdate();
+    },
+    [model]
+  );
+
+  return [model.current, applyPatch];
 }
 
-export function joinUrl(base, tail) {
-  return tail.startsWith("./")
-    ? (base.endsWith("/") ? base.slice(0, -1) : base) + tail.slice(1)
-    : tail;
+function useForceUpdate() {
+  const [, updateState] = React.useState();
+  return React.useCallback(() => updateState({}), []);
 }
