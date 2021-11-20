b9a388f4f45078cef05ebdadff91932f54a3d012
rmorshea <ryan.morshead@gmail.com>
2021-11-20 12:27:37 -0800

    do not use SO_REUSEADDR when testing
    this can lead to unexpected behavior when trying
    to choose a free port.

--- src/idom/server/utils.py
+++ src/idom/server/utils.py
@@ -109,12 +109,23 @@ def find_builtin_server_type(type_name: str) -> ServerFactory[Any, Any]:
             )
 
 
-def find_available_port(host: str, port_min: int = 8000, port_max: int = 9000) -> int:
+def find_available_port(
+    host: str,
+    port_min: int = 8000,
+    port_max: int = 9000,
+    allow_reuse_waiting_ports: bool = True,
+) -> int:
     """Get a port that's available for the given host and port range"""
     for port in range(port_min, port_max):
         with closing(socket.socket()) as sock:
             try:
-                sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                if allow_reuse_waiting_ports:
+                    # As per this answer: https://stackoverflow.com/a/19247688/3159288
+                    # setting can be somewhat unreliable because we allow the use of
+                    # ports that are stuck in TIME_WAIT. However, not setting the option
+                    # means we're overly cautious and almost always use a different addr
+                    # even if it could have actually been used.
+                    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
                 sock.bind((host, port))
             except OSError:
                 pass
