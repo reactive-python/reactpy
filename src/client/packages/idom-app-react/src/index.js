8b61b38dcdbf86e1871b3e4926d780888c50be96
liberty-rising <rwyszomirski@dyvenia.com>
2022-03-23 09:37:54 +0100

    add console log

--- src/client/packages/idom-app-react/src/index.js
+++ src/client/packages/idom-app-react/src/index.js
@@ -1,6 +1,7 @@
 import { mountWithLayoutServer, LayoutServerInfo } from "idom-client-react";
 
 export function mount(mountPoint) {
+  console.log("Mounted");
   const serverInfo = new LayoutServerInfo({
     host: document.location.hostname,
     port: document.location.port,
