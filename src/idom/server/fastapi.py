90c89be312b6e56b7c93d25c901d7822d59aa7d2
rmorshea <ryan.morshead@gmail.com>
2022-03-27 16:24:17 -0700

    fix ref

--- src/idom/server/fastapi.py
+++ src/idom/server/fastapi.py
@@ -9,18 +9,18 @@ from . import starlette
 
 
 serve_development_app = starlette.serve_development_app
-"""Alias for :func:`starlette.serve_development_app`"""
+"""Alias for :func:`idom.server.starlette.serve_development_app`"""
 
 # see: https://github.com/idom-team/flake8-idom-hooks/issues/12
 use_scope = starlette.use_scope  # noqa: ROH101
-"""Alias for :func:`starlette.use_scope`"""
+"""Alias for :func:`idom.server.starlette.use_scope`"""
 
 # see: https://github.com/idom-team/flake8-idom-hooks/issues/12
 use_websocket = starlette.use_websocket  # noqa: ROH101
-"""Alias for :func:`starlette.use_websocket`"""
+"""Alias for :func:`idom.server.starlette.use_websocket`"""
 
 Options = starlette.Options
-"""Alias for :class:`starlette.Options`"""
+"""Alias for :class:`idom.server.starlette.Options`"""
 
 
 def configure(
