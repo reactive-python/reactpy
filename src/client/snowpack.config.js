ac951e592cb3bda0da4fe48bf431aed55ee32229
rmorshea <ryan.morshead@gmail.com>
2022-03-27 20:03:49 -0700

    experiment with bundling

--- src/client/snowpack.config.js
+++ src/client/snowpack.config.js
@@ -2,4 +2,6 @@ module.exports = {
   workspaceRoot: false,
   testOptions: { files: ["**/tests/**/*", "**/*.test.*"] },
   buildOptions: { out: "../idom/client" },
+  mount: { public: { url: "/", static: true } },
+  optimize: { bundle: true, target: "es2018" },
 };
