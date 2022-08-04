f0a3220ebda1c44bd7afa515634d43b11c1e1665
Archmonger <16909269+Archmonger@users.noreply.github.com>
2022-08-04 01:55:48 -0700

    better interface for html_to_vdom

--- src/idom/utils.py
+++ src/idom/utils.py
@@ -52,63 +52,81 @@ class Ref(Generic[_RefValue]):
         return f"{type(self).__name__}({current})"
 
 
-def _set_if_val_exists(object, key, value):
-    """Sets a key on a dictionary if the value's length is greater than 0."""
-    if len(value):
-        object[key] = value
+def html_to_vdom(html: str, *transforms: _ModelTransform):
+    """Transform HTML into a DOM model
+    Parameters:
+        source:
+            The raw HTML as a string
+        transforms:
+            Functions of the form ``transform(old) -> new`` where ``old`` is a VDOM
+            dictionary which will be replaced by ``new``. For example, you could use a
+            transform function to add highlighting to a ``<code/>`` block.
+    """
+
+    if not isinstance(html, str):
+        raise TypeError("html_to_vdom expects a string!")
+
+    return HtmlToVdom().convert(html, *transforms)
 
 
-def _vdom_attributes(object):
-    if "attributes" in object and "style" in object["attributes"]:
-        style = object["attributes"]["style"]
-        if isinstance(style, str):
-            style_dict = {}
-            for k, v in (part.split(":", 1) for part in style.split(";") if part):
-                title_case_key = k.title().replace("-", "")
-                camel_case_key = title_case_key[:1].lower() + title_case_key[1:]
-                style_dict[camel_case_key] = v
-            object["attributes"]["style"] = style_dict
+class HtmlToVdom:
+    def convert(self, html: Union[str, etree._Element], *transforms: _ModelTransform):
+        """Convert an lxml.etree node tree into a VDOM dict."""
+        # Keep track of whether this is the root node
+        root_node = False
 
+        # If the user provided a string, convert it to an lxml.etree node.
+        if isinstance(html, str):
+            parser = etree.HTMLParser()
+            node = fragment_fromstring(html, create_parent=True, parser=parser)
+            root_node = True
+        elif isinstance(html, etree._Element):
+            node = html
+        else:
+            raise TypeError("html_to_vdom expects a string or lxml.etree._Element")
 
-def _vdom_key(object):
-    if object["tagName"] == "script":
-        if not isinstance(object["children"][0], str):
-            # The script tag contents should be the first child
-            raise TypeError("Could not find script tag contents!")
-        if object["children"][0]:
-            object["key"] = object["children"][0]
+        # Recursively convert the lxml.etree node to a VDOM dict.
+        vdom = {"tagName": node.tag}
+        node_children = [node.text] if node.text else []
+        node_children.extend([self.convert(child) for child in node.iterchildren(None)])
+        self._set_if_val_exists(vdom, "children", node_children)
+        self._set_if_val_exists(vdom, "attributes", dict(node.items()))
+        self._vdom_attributes(vdom)
+        self._vdom_key(vdom)
 
+        # Apply any provided transforms.
+        for transform in transforms:
+            vdom = transform(vdom)
 
-def html_to_vdom(html: Union[str, etree._Element], *transforms: _ModelTransform):
-    """Convert an lxml.etree node tree into a VDOM dict."""
-    # Keep track of whether this is the root node
-    root_node = False
+        # The root node is always a React Fragment
+        if root_node:
+            vdom["tagName"] = ""
 
-    # If the user provided a string, convert it to an lxml.etree node.
-    if isinstance(html, str):
-        parser = etree.HTMLParser()
-        node = fragment_fromstring(html, create_parent=True, parser=parser)
-        root_node = True
-    elif isinstance(html, etree._Element):
-        node = html
-    else:
-        raise TypeError("html_to_vdom expects a string or lxml.etree._Element")
+        return vdom
 
-    # Convert the lxml.etree node to a VDOM dict.
-    vdom = {"tagName": node.tag}
-    node_children = [node.text] if node.text else []
-    node_children.extend([html_to_vdom(child) for child in node.iterchildren(None)])
-    _set_if_val_exists(vdom, "children", node_children)
-    _set_if_val_exists(vdom, "attributes", dict(node.items()))
-    _vdom_attributes(vdom)
-    _vdom_key(vdom)
+    @staticmethod
+    def _set_if_val_exists(object, key, value):
+        """Sets a key on a dictionary if the value's length is greater than 0."""
+        if len(value):
+            object[key] = value
 
-    # Apply any provided transforms.
-    for transform in transforms:
-        vdom = transform(vdom)
+    @staticmethod
+    def _vdom_attributes(object):
+        if "attributes" in object and "style" in object["attributes"]:
+            style = object["attributes"]["style"]
+            if isinstance(style, str):
+                style_dict = {}
+                for k, v in (part.split(":", 1) for part in style.split(";") if part):
+                    title_case_key = k.title().replace("-", "")
+                    camel_case_key = title_case_key[:1].lower() + title_case_key[1:]
+                    style_dict[camel_case_key] = v
+                object["attributes"]["style"] = style_dict
 
-    # The root node is always a React Fragment
-    if root_node:
-        vdom["tagName"] = ""
-
-    return vdom
+    @staticmethod
+    def _vdom_key(object):
+        if object["tagName"] == "script":
+            if not isinstance(object["children"][0], str):
+                # The script tag contents should be the first child
+                raise TypeError("Could not find script tag contents!")
+            if object["children"][0]:
+                object["key"] = object["children"][0]
