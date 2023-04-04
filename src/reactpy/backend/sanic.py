14c4da56f46548796bb0b49cbdf627c1a38acebf
Archmonger <16909269+Archmonger@users.noreply.github.com>
2023-04-04 15:09:45 -0700

    fix style

--- src/reactpy/backend/sanic.py
+++ src/reactpy/backend/sanic.py
@@ -4,7 +4,7 @@ import asyncio
 import json
 import logging
 from dataclasses import dataclass
-from typing import Any, MutableMapping, Tuple
+from typing import Any, Tuple
 from urllib import parse as urllib_parse
 from uuid import uuid4
 
