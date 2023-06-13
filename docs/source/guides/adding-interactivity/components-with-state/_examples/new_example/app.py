5a4bc62a9bfb51c4929a6315ddb089e95797ccdf
josephlarrivy <joseph.larrivy@gmail.com>
2023-06-13 17:12:23 -0500

    hook needs some cleanup, but working for now

--- docs/source/guides/adding-interactivity/components-with-state/_examples/new_example/app.py
+++ docs/source/guides/adding-interactivity/components-with-state/_examples/new_example/app.py
@@ -2,30 +2,32 @@ from reactpy import component, hooks, html
 from reactpy.backend.flask import configure
 from flask import Flask
 
-from use_counting_hook import use_counting_hook
+from use_counting_hook import my_first_reactpy_hook
 
-# def reducer(count, action):
-#     if action == "increment":
-#         return count + 1
-#     elif action == "decrement":
-#         return count - 1
-#     elif action == "reset":
-#         return 0
-#     else:
-#         msg = f"Unknown action '{action}'"
-#         raise ValueError(msg)
 
+
+
+# @component
+# def Example_Component():
+
+#     count, dispatch = use_counting_hook(0)
+
+#     return html.div(
+#         html.button({"on_click": lambda event: dispatch("reset")}, "Reset"),
+#         html.button({"on_click": lambda event: dispatch("increment")}, "add"),
+#         html.button({"on_click": lambda event: dispatch("decrement")}, "subtract"),
+#         f"Count: {count}"
+#     )
 
 @component
 def Example_Component():
 
-    # count, dispatch = hooks.use_reducer(reducer, 0)
-    count, dispatch = use_counting_hook(0)
+    count, increment, decrement, reset = my_first_reactpy_hook(10)
 
     return html.div(
-        html.button({"on_click": lambda event: dispatch("reset")}, "Reset"),
-        html.button({"on_click": lambda event: dispatch("increment")}, "add"),
-        html.button({"on_click": lambda event: dispatch("decrement")}, "subtract"),
+        html.button({"on_click": lambda event: reset()}, "Reset"),
+        html.button({"on_click": lambda event: increment()}, "add"),
+        html.button({"on_click": lambda event: decrement()}, "subtract"),
         f"Count: {count}"
     )
 
--- docs/source/guides/adding-interactivity/components-with-state/_examples/new_example/use_counting_hook.py
+++ docs/source/guides/adding-interactivity/components-with-state/_examples/new_example/use_counting_hook.py
@@ -1,15 +1,41 @@
 from reactpy import hooks
 
-def reducer(count, action):
-    if action == "increment":
-        return count + 1
-    elif action == "decrement":
-        return count - 1
-    elif action == "reset":
-        return 0
-    else:
-        msg = f"Unknown action '{action}'"
-        raise ValueError(msg)
+# def reducer(count, action):
+#     if action == "increment":
+#         return count + 1
+#     elif action == "decrement":
+#         return count - 1
+#     elif action == "reset":
+#         return 0
+#     else:
+#         msg = f"Unknown action '{action}'"
+#         raise ValueError(msg)
 
-def use_counting_hook(initial_value=0):
-    return hooks.use_reducer(reducer, initial_value)
+# def use_counting_hook(initial_value=0):
+#     return hooks.use_reducer(reducer, initial_value)
+
+
+def my_first_reactpy_hook(initial_value=0):
+    def reducer(count, action):
+        if action == "increment":
+            return count + 1
+        elif action == "decrement":
+            return count - 1
+        elif action == "reset":
+            return initial_value
+        else:
+            msg = f"Unknown action '{action}'"
+            raise ValueError(msg)
+
+    count, dispatch = hooks.use_reducer(reducer, initial_value)
+
+    def increment():
+        dispatch("increment")
+
+    def decrement():
+        dispatch("decrement")
+
+    def reset():
+        dispatch("reset")
+
+    return count, increment, decrement, reset
