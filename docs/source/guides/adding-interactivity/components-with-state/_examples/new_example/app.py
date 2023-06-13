5ac407e20da1fdd542807d068e47cde747ec5607
josephlarrivy <joseph.larrivy@gmail.com>
2023-06-13 16:33:59 -0500

    basics set up using use_reducer

--- docs/source/guides/adding-interactivity/components-with-state/_examples/new_example/app.py
+++ docs/source/guides/adding-interactivity/components-with-state/_examples/new_example/app.py
@@ -2,18 +2,30 @@ from reactpy import component, hooks, html
 from reactpy.backend.flask import configure
 from flask import Flask
 
-from use_counting_hook import use_counting_hook
+# from use_counting_hook import use_counting_hook
+
+def reducer(count, action):
+    if action == "increment":
+        return count + 1
+    elif action == "decrement":
+        return count - 1
+    elif action == "reset":
+        return 0
+    else:
+        msg = f"Unknown action '{action}'"
+        raise ValueError(msg)
 
 
 @component
 def Example_Component():
 
-    number, number, increment, decrement = hooks.use_counting_hook(0)
+    count, dispatch = hooks.use_reducer(reducer, 0)
 
     return html.div(
-        html.button({"on_click": handle_next_click}, "Next"),
-        html.h2(f"{number}"),
-        html.h2("test")
+        f"Count: {count}",
+        html.button({"on_click": lambda event: dispatch("reset")}, "Reset"),
+        html.button({"on_click": lambda event: dispatch("increment")}, "+"),
+        html.button({"on_click": lambda event: dispatch("decrement")}, "-"),
     )
 
 
