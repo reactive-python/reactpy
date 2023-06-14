1b42e32e443f3c271f66987cce8123c2ba800c8b
Archmonger <16909269+Archmonger@users.noreply.github.com>
2023-06-13 22:05:00 -0700

    add no cov pragma

--- src/py/reactpy/reactpy/backend/__init__.py
+++ src/py/reactpy/reactpy/backend/__init__.py
@@ -1,12 +1,13 @@
 import mimetypes
 
 # Fix for issue where Windows registry gets corrupt and mime types go missing
-mimetypes.init()
+if not mimetypes.inited: # pragma: no cover
+    mimetypes.init()
 MIME_TYPES = {
     ".js": "application/javascript",
     ".css": "text/css",
     ".json": "application/json",
 }
 for extension, mime_type in MIME_TYPES.items():
-    if not mimetypes.types_map.get(extension):
+    if not mimetypes.types_map.get(extension): # pragma: no cover
         mimetypes.add_type(mime_type, extension)
