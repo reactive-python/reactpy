d0c4d6ad2593de6240086fa77c734b2a08b91c07
Archmonger <16909269+Archmonger@users.noreply.github.com>
2023-06-13 21:51:07 -0700

    use static mime types

--- src/py/reactpy/reactpy/backend/__init__.py
+++ src/py/reactpy/reactpy/backend/__init__.py
@@ -2,6 +2,16 @@ import mimetypes
 
 # Fix for issue where Windows registry gets corrupt and mime types go missing
 mimetypes.init()
-for extension, mime_type in mimetypes.common_types.items():
+MIME_TYPES = {
+    ".js": "application/javascript",
+    ".css": "text/css",
+    ".ico": "image/x-icon",
+    ".png": "image/png",
+    ".jpg": "image/jpeg",
+    ".jpeg": "image/jpeg",
+    ".svg": "image/svg+xml",
+    ".json": "application/json",
+}
+for extension, mime_type in MIME_TYPES.items():
     if not mimetypes.types_map.get(extension):
         mimetypes.add_type(mime_type, extension)
