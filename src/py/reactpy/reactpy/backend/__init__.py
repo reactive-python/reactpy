7303c27d24a67cfa20f2cfe2082771b5ba0caecc
Archmonger <16909269+Archmonger@users.noreply.github.com>
2023-06-13 21:37:17 -0700

    fix lint

--- src/py/reactpy/reactpy/backend/__init__.py
+++ src/py/reactpy/reactpy/backend/__init__.py
@@ -2,6 +2,6 @@ import mimetypes
 
 # Fix for issue where Windows registry gets corrupt and mime types go missing
 mimetypes.init()
-for extension, type in mimetypes.common_types.items():
+for extension, mime_type in mimetypes.common_types.items():
     if not mimetypes.types_map.get(extension):
-        mimetypes.add_type(type, extension)
+        mimetypes.add_type(mime_type, extension)
