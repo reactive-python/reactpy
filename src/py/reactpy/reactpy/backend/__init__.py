a6f7341c08fea8ce3da5afda415df6c779dd3733
Archmonger <16909269+Archmonger@users.noreply.github.com>
2023-06-13 22:07:00 -0700

    format

--- src/py/reactpy/reactpy/backend/__init__.py
+++ src/py/reactpy/reactpy/backend/__init__.py
@@ -1,7 +1,7 @@
 import mimetypes
 
 # Fix for issue where Windows registry gets corrupt and mime types go missing
-if not mimetypes.inited: # pragma: no cover
+if not mimetypes.inited:  # pragma: no cover
     mimetypes.init()
 MIME_TYPES = {
     ".js": "application/javascript",
@@ -9,5 +9,5 @@ MIME_TYPES = {
     ".json": "application/json",
 }
 for extension, mime_type in MIME_TYPES.items():
-    if not mimetypes.types_map.get(extension): # pragma: no cover
+    if not mimetypes.types_map.get(extension):  # pragma: no cover
         mimetypes.add_type(mime_type, extension)
