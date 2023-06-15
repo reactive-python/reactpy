04fccde061f555d52f81693a6fbe3feab3ea0fd6
Archmonger <16909269+Archmonger@users.noreply.github.com>
2023-06-14 21:10:28 -0700

    OS-neutral comment

--- src/py/reactpy/reactpy/backend/__init__.py
+++ src/py/reactpy/reactpy/backend/__init__.py
@@ -3,7 +3,7 @@ from logging import getLogger
 
 _logger = getLogger(__name__)
 
-# Fix for missing mime types due to corrupt Windows registry
+# Fix for missing mime types due to OS corruption/misconfiguration
 if not mimetypes.inited:
     mimetypes.init()
 MIME_TYPES = {
