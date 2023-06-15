0a59c46bc0c7a1b94dd7a38edd2a469f4f89b5b1
Archmonger <16909269+Archmonger@users.noreply.github.com>
2023-06-15 02:24:00 -0700

    websocket URL

--- src/py/reactpy/reactpy/templatetag.py
+++ src/py/reactpy/reactpy/templatetag.py
@@ -5,9 +5,11 @@ import urllib.parse
 REACTPY_WEBSOCKET_URL: str
 REACTPY_WEB_MODULES_URL: str
 REACTPY_RECONNECT_MAX: str
-REACTPY_CLIENT_URL_PATH: str
+REACTPY_CLIENT_URL: str
+
 
 class ComponentTag(StandaloneTag):
+    """This allows enables a `component` tag to be used in any Jinja2 rendering context."""
     safe_output = True
     tags = {"component"}
 
@@ -15,22 +17,25 @@ class ComponentTag(StandaloneTag):
         uuid = uuid4().hex
         class_ = kwargs.pop("class", "")
         kwargs.pop("key", "")  # `key` is effectively useless for the root node
-        url_path = REACTPY_CLIENT_URL_PATH
+
+        # Generate the websocket URL
+        # TODO: This will require rewriting the websocket URL to `reactpy-ws/<uuid>?<kwargs>`
+        component_ws_url = f"{REACTPY_WEBSOCKET_URL}/{uuid}"
         if kwargs.get("args") is not None:
             raise ValueError("Cannot specify `args` as a keyword argument")
         if args:
             kwargs["args"] = args
         if kwargs:
-            url_path += f"?{urllib.parse.urlencode(kwargs)}"
+            component_ws_url += f"?{urllib.parse.urlencode(kwargs)}"
 
         return (
             f'<div id="{ uuid }" class="{ class_ }"></div>'
             '<script type="module" crossorigin="anonymous">'
-            f'import {{ mountViewToElement }} from "{url_path}";'
+            f'import {{ mountViewToElement }} from "{REACTPY_CLIENT_URL}";'
             f'const mountElement = document.getElementById("{ uuid }");'
             "mountViewToElement("
             "    mountElement,"
-            f'   "{ REACTPY_WEBSOCKET_URL }",'
+            f'   "{ component_ws_url }",'
             f'   "{ REACTPY_WEB_MODULES_URL }",'
             f'   "{ REACTPY_RECONNECT_MAX }",'
             f'   "{ dotted_path }",'
