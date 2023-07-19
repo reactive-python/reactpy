3d974307b4a39cd5aeea2843e4e956ca01f61afd
Archmonger <16909269+Archmonger@users.noreply.github.com>
2023-07-19 00:25:08 -0700

    refactoring

--- src/py/reactpy/reactpy/backend/asgi.py
+++ src/py/reactpy/reactpy/backend/asgi.py
@@ -51,7 +51,7 @@ class ReactPy:
         )
         if not self.component and not self.user_app:
             raise TypeError(
-                "The first argument to `ReactPy` must be a component or an ASGI application."
+                "The first argument to ReactPy(...) must be a component or an ASGI application."
             )
         self.dispatch_path = re.compile(dispatcher_path)
         self.js_modules_path = re.compile(js_modules_path) if js_modules_path else None
@@ -64,93 +64,63 @@ class ReactPy:
         )
         self.head = vdom_head_elements_to_html(head)
         self._cached_index_html = ""
+        self.connected = False
 
     async def __call__(self, scope, receive, send) -> None:
         """The ASGI callable. This determines whether ReactPy should route the the
         request to ourselves or to the user application."""
-
         # Determine if ReactPy should handle the request
         if not self.user_app or re.match(self.all_paths, scope["path"]):
-            # Dispatch a Python component
-            if scope["type"] == "websocket" and re.match(
-                self.dispatch_path, scope["path"]
-            ):
-                await self.component_dispatch_app(scope, receive, send)
-                return
-
-            # User tried to use an unsupported HTTP method
-            if scope["type"] == "http" and scope["method"] not in ("GET", "HEAD"):
-                await simple_response(
-                    scope, send, status=405, content="Method Not Allowed"
-                )
-                return
-
-            # Route requests to our JS web module app
-            if (
-                scope["type"] == "http"
-                and self.js_modules_path
-                and re.match(self.js_modules_path, scope["path"])
-            ):
-                await self.js_modules_app(scope, receive, send)
-                return
-
-            # Route requests to our static file server app
-            if (
-                scope["type"] == "http"
-                and self.static_path
-                and re.match(self.static_path, scope["path"])
-            ):
-                await self.static_file_app(scope, receive, send)
-                return
-
-            # Route all other requests to serve a component (user is in standalone mode)
-            if scope["type"] == "http" and self.component:
-                await self.index_html_app(scope, receive, send)
-                return
+            await self.reactpy_app(scope, receive, send)
+            return
 
         # Serve the user's application
-        if self.user_app:
-            await self.user_app(scope, receive, send)
-            return
+        await self.user_app(scope, receive, send)
 
         _logger.error("ReactPy appears to be misconfigured. Request not handled.")
 
+    async def reactpy_app(self, scope, receive, send) -> None:
+        """Determine what type of request this is and route it to the appropriate
+        ReactPy ASGI sub-application."""
+
+        # Only HTTP and WebSocket requests are supported
+        if scope["type"] not in {"http", "websocket"}:
+            return
+
+        # Dispatch a Python component
+        if scope["type"] == "websocket" and re.match(self.dispatch_path, scope["path"]):
+            await self.component_dispatch_app(scope, receive, send)
+            return
+
+        # Only HTTP GET and HEAD requests are supported
+        if scope["method"] not in {"GET", "HEAD"}:
+            await http_response(scope, send, 405, "Method Not Allowed")
+            return
+
+        # JS modules app
+        if self.js_modules_path and re.match(self.js_modules_path, scope["path"]):
+            await self.js_modules_app(scope, receive, send)
+            return
+
+        # Static file app
+        if self.static_path and re.match(self.static_path, scope["path"]):
+            await self.static_file_app(scope, receive, send)
+            return
+
+        # Standalone app: Serve a single component using index.html
+        if self.component:
+            await self.standalone_app(scope, receive, send)
+            return
+
     async def component_dispatch_app(self, scope, receive, send) -> None:
-        """The ASGI application for ReactPy Python components."""
-
-        parsed_url = urllib.parse.urlparse(scope["path"])
-
-        # If in standalone mode, serve the user provided component.
-        # In middleware mode, get the component from the URL.
-        component = self.component or re.match(self.dispatch_path, scope["path"])[1]
-
+        """ASGI app for rendering ReactPy Python components."""
         while True:
             event = await receive()
 
-            if event["type"] == "websocket.connect":
+            if event["type"] == "websocket.connect" and not self.connected:
+                self.connected = True
                 await send({"type": "websocket.accept"})
-                self.recv_queue: asyncio.Queue = asyncio.Queue()
-                await serve_layout(
-                    Layout(
-                        ConnectionContext(
-                            component(),
-                            value=Connection(
-                                scope=scope,
-                                location=Location(
-                                    parsed_url.path,
-                                    f"?{parsed_url.query}" if parsed_url.query else "",
-                                ),
-                                carrier={
-                                    "scope": scope,
-                                    "send": send,
-                                    "receive": receive,
-                                },
-                            ),
-                        )
-                    ),
-                    send_json(send),
-                    self.recv_queue.get,
-                )
+                await self.run_dispatcher(scope, receive, send)
 
             if event["type"] == "websocket.disconnect":
                 break
@@ -159,8 +129,7 @@ class ReactPy:
                 await self.recv_queue.put(orjson.loads(event["text"]))
 
     async def js_modules_app(self, scope, receive, send) -> None:
-        """The ASGI application for ReactPy web modules."""
-
+        """ASGI app for ReactPy web modules."""
         if not REACTPY_WEB_MODULES_DIR.current:
             raise RuntimeError("No web modules directory configured.")
 
@@ -172,15 +141,14 @@ class ReactPy:
                 re.match(self.js_modules_path, scope["path"])[1],
             )
         except ValueError:
-            await simple_response(send, 403, "Forbidden")
+            await http_response(scope, send, 403, "Forbidden")
             return
 
         # Serve the file
         await file_response(scope, send, abs_file_path)
 
     async def static_file_app(self, scope, receive, send) -> None:
-        """The ASGI application for ReactPy static files."""
-
+        """ASGI app for ReactPy static files."""
         if not self.static_dir:
             raise RuntimeError(
                 "Static files cannot be served without defining `static_dir`."
@@ -193,17 +161,14 @@ class ReactPy:
                 re.match(self.static_path, scope["path"])[1],
             )
         except ValueError:
-            await simple_response(send, 403, "Forbidden")
+            await http_response(scope, send, 403, "Forbidden")
             return
 
         # Serve the file
         await file_response(scope, send, abs_file_path)
 
-    async def index_html_app(self, scope, receive, send) -> None:
-        """The ASGI application for ReactPy index.html."""
-
-        # TODO: We want to respect the if-modified-since header, but currently can't
-        # due to the fact that our HTML is not statically rendered.
+    async def standalone_app(self, scope, receive, send) -> None:
+        """ASGI app for ReactPy standalone mode."""
         file_path = CLIENT_BUILD_DIR / "index.html"
         if not self._cached_index_html:
             async with aiofiles.open(file_path, "rb") as file_handle:
@@ -211,24 +176,46 @@ class ReactPy:
                     __head__=self.head
                 )
 
-        # Head requests don't need a body
-        if scope["method"] == "HEAD":
-            await simple_response(
-                send,
-                200,
-                "",
-                content_type=b"text/html",
-                headers=[(b"cache-control", b"no-cache")],
-            )
-            return
-
         # Send the index.html
-        await simple_response(
+        await http_response(
+            scope,
             send,
             200,
             self._cached_index_html,
             content_type=b"text/html",
-            headers=[(b"cache-control", b"no-cache")],
+            headers=[
+                (b"content-length", len(self._cached_index_html)),
+                (b"etag", hash(self._cached_index_html)),
+            ],
+        )
+
+    async def run_dispatcher(self, scope, receive, send):
+        # If in standalone mode, serve the user provided component.
+        # In middleware mode, get the component from the URL.
+        component = self.component or re.match(self.dispatch_path, scope["path"])[1]
+        parsed_url = urllib.parse.urlparse(scope["path"])
+        self.recv_queue: asyncio.Queue = asyncio.Queue()
+
+        await serve_layout(
+            Layout(
+                ConnectionContext(
+                    component(),
+                    value=Connection(
+                        scope=scope,
+                        location=Location(
+                            parsed_url.path,
+                            f"?{parsed_url.query}" if parsed_url.query else "",
+                        ),
+                        carrier={
+                            "scope": scope,
+                            "send": send,
+                            "receive": receive,
+                        },
+                    ),
+                )
+            ),
+            send_json(send),
+            self.recv_queue.get,
         )
 
 
@@ -241,7 +228,8 @@ def send_json(send) -> None:
     return _send_json
 
 
-async def simple_response(
+async def http_response(
+    scope,
     send,
     code: int,
     message: str,
@@ -249,7 +237,6 @@ async def simple_response(
     headers: Sequence = (),
 ) -> None:
     """Send a simple response."""
-
     await send(
         {
             "type": "http.response.start",
@@ -257,28 +244,28 @@ async def simple_response(
             "headers": [(b"content-type", content_type, *headers)],
         }
     )
-    await send({"type": "http.response.body", "body": message.encode()})
+    # Head requests don't need a body
+    if scope["method"] != "HEAD":
+        await send({"type": "http.response.body", "body": message.encode()})
 
 
 async def file_response(scope, send, file_path: Path) -> None:
     """Send a file in chunks."""
-
     # Make sure the file exists
     if not await asyncio.to_thread(os.path.exists, file_path):
-        await simple_response(send, 404, "File not found.")
+        await http_response(scope, send, 404, "File not found.")
         return
 
     # Make sure it's a file
     if not await asyncio.to_thread(os.path.isfile, file_path):
-        await simple_response(send, 400, "Not a file.")
+        await http_response(scope, send, 400, "Not a file.")
         return
 
     # Check if the file is already cached by the client
-    etag = await get_val_from_header(scope, b"ETag")
-    if etag and etag != await asyncio.to_thread(
-        os.path.getmtime, file_path
-    ):
-        await simple_response(send, 304, "Not modified.")
+    etag = await get_val_from_header(scope, b"etag")
+    modification_time = await asyncio.to_thread(os.path.getmtime, file_path)
+    if etag and etag != modification_time:
+        await http_response(scope, send, 304, "Not modified.")
         return
 
     # Get the file's MIME type
@@ -294,6 +281,7 @@ async def file_response(scope, send, file_path: Path) -> None:
         )
 
     # Send the file in chunks
+    file_size = await asyncio.to_thread(os.path.getsize, file_path)
     async with aiofiles.open(file_path, "rb") as file_handle:
         await send(
             {
@@ -301,39 +289,32 @@ async def file_response(scope, send, file_path: Path) -> None:
                 "status": 200,
                 "headers": [
                     (b"content-type", mime_type.encode()),
-                    (
-                        b"ETag",
-                        str(
-                            await asyncio.to_thread(os.path.getmtime, file_path)
-                        ).encode(),
-                    ),
+                    (b"etag", modification_time),
+                    (b"content-length", file_size),
                 ],
             }
         )
 
         # Head requests don't need a body
-        if scope["method"] == "HEAD":
-            return
-
-        while True:
-            chunk = await file_handle.read(DEFAULT_BLOCK_SIZE)
-            more_body = bool(chunk)
-            await send(
-                {
-                    "type": "http.response.body",
-                    "body": chunk,
-                    "more_body": more_body,
-                }
-            )
-            if not more_body:
-                break
+        if scope["method"] != "HEAD":
+            while True:
+                chunk = await file_handle.read(DEFAULT_BLOCK_SIZE)
+                more_body = bool(chunk)
+                await send(
+                    {
+                        "type": "http.response.body",
+                        "body": chunk,
+                        "more_body": more_body,
+                    }
+                )
+                if not more_body:
+                    break
 
 
 async def get_val_from_header(
     scope: dict, key: str, default: str | None = None
 ) -> str | None:
     """Get a value from a scope's headers."""
-
     return await anext(
         (
             value.decode()
