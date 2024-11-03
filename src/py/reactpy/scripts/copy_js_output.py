be50b136d829da1143b9b9c63977bf2d34918fa7
Archmonger <16909269+Archmonger@users.noreply.github.com>
2024-11-02 21:57:57 -0700

    noqa for print

--- src/py/reactpy/scripts/copy_js_output.py
+++ src/py/reactpy/scripts/copy_js_output.py
@@ -5,4 +5,4 @@ output_dir = Path(__file__).parent.parent / "reactpy" / "_static"
 source_dir = Path(__file__).parent.parent.parent.parent / "js" / "app" / "dist"
 rmtree(output_dir, ignore_errors=True)
 copytree(source_dir, output_dir)
-print("JavaScript output copied to reactpy/_static")
+print("JavaScript output copied to reactpy/_static")  # noqa: T201
