fe848082208f02b0a33bc33be9dd9f6a2b28e5db
Archmonger <16909269+Archmonger@users.noreply.github.com>
2025-01-14 17:12:33 -0800

    Seemingly fully functional Python build?

--- src/build_scripts/copy_js_output.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-from shutil import copytree, rmtree
-
-output_dir = Path(__file__).parent.parent / "reactpy" / "static"
-source_dir = Path(__file__).parent.parent / "js" / "app" / "dist"
-rmtree(output_dir, ignore_errors=True)
-copytree(source_dir, output_dir)
-print("JavaScript output copied to reactpy/static")  # noqa: T201
--- src/js/packages/@reactpy/app/src/index.ts
+++ src/js/packages/@reactpy/app/src/index.ts
@@ -1,6 +1,6 @@
 import { mount, SimpleReactPyClient } from "@reactpy/client";
 
-export function app(element: HTMLElement) {
+function app(element: HTMLElement) {
   const client = new SimpleReactPyClient({
     serverLocation: {
       url: document.location.origin,
@@ -9,4 +9,12 @@ export function app(element: HTMLElement) {
     },
   });
   mount(element, client);
+}
+
+let element = document.getElementById("app");
+if (element){
+  app(element);
+}
+else {
+  console.error("Element with id 'app' not found");
 }
