ca8839498c02cfcd86ba090743cd00f58dafd6c5
Archmonger <16909269+Archmonger@users.noreply.github.com>
2025-01-27 01:38:56 -0800

    Revert changes to copy_dir.py

--- src/build_scripts/copy_dir.py
+++ src/build_scripts/copy_dir.py
@@ -5,39 +5,36 @@
 
 # ruff: noqa: INP001
 import logging
-import re
 import shutil
 import sys
 from pathlib import Path
 
 
-def copy_files(source: Path, destination: Path, pattern: str) -> None:
+def copy_files(source: Path, destination: Path) -> None:
     if destination.exists():
         shutil.rmtree(destination)
     destination.mkdir()
 
     for file in source.iterdir():
         if file.is_file():
-            if not pattern or re.match(pattern, file.name):
-                shutil.copy(file, destination / file.name)
+            shutil.copy(file, destination / file.name)
         else:
-            copy_files(file, destination / file.name, pattern)
+            copy_files(file, destination / file.name)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) not in {3, 4}:
+    if len(sys.argv) != 3:  # noqa
         logging.error(
-            "Script used incorrectly!\nUsage: python copy_dir.py <source_dir> <destination> <optional:match_pattern>"
+            "Script used incorrectly!\nUsage: python copy_dir.py <source_dir> <destination>"
         )
         sys.exit(1)
 
     root_dir = Path(__file__).parent.parent.parent
-    _source = Path(root_dir / sys.argv[1])
-    _destintation = Path(root_dir / sys.argv[2])
-    _pattern = sys.argv[3] if len(sys.argv) == 4 else ""  # noqa
+    src = Path(root_dir / sys.argv[1])
+    dest = Path(root_dir / sys.argv[2])
 
-    if not _source.exists():
-        logging.error("Source directory %s does not exist", _source)
+    if not src.exists():
+        logging.error("Source directory %s does not exist", src)
         sys.exit(1)
 
-    copy_files(_source, _destintation, _pattern)
+    copy_files(src, dest)
