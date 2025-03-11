35f9e7d3587949ee7cfb0ff906b968c5484c4a49
Shawn Crawley <shawncrawley@gmail.com>
2025-03-11 06:42:17 -0600

    Handles fix on server-side rather than client-side

--- src/js/packages/@reactpy/client/src/vdom.tsx
+++ src/js/packages/@reactpy/client/src/vdom.tsx
@@ -142,14 +142,6 @@ export function createAttributes(
   model: ReactPyVdom,
   client: ReactPyClientInterface,
 ): { [key: string]: any } {
-  // Add key to attributes
-  if (model.key) {
-    if (model.attributes) {
-      model.attributes.key = model.key;
-    } else {
-      model.attributes = { key: model.key };
-    }
-  }
   return Object.fromEntries(
     Object.entries({
       // Normal HTML attributes
